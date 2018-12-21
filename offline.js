834e90e3c9f374eb03ceec9b69506c07b9f9a33d
Vaughan Po <vaughan188@gmail.com>
2018-12-21 23:45:20 +0530

    first commit

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1545415804,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/player_1-sheet0.png",
+		"images/bullet_1-sheet0.png",
+		"images/shooter-sheet0.png",
+		"images/player_2-sheet0.png",
+		"images/bullet_2-sheet0.png",
+		"images/explossion-sheet0.png",
+		"images/explosion-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
